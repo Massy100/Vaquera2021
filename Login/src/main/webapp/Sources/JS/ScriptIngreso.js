55e908d6c8ff8505cb42caa7f8546dea7c93b3d9
Massy100 <80069634+Massy100@users.noreply.github.com>
2021-12-09 16:38:54 -0600

    Cuarto commit
    Todo funciona bien, agregue algunos detalles esteticos

--- Login/src/main/webapp/Sources/JS/ScriptIngreso.js
+++ Login/src/main/webapp/Sources/JS/ScriptIngreso.js
@@ -3,12 +3,12 @@ document.onload = init;
 function init() {
     document.getElementById('add').onclick = add;
 }
+
 function guardar() {
     var nombre = document.getElementById("nombre").value;
     var apellido = document.getElementById("apellido").value;
     var cui = document.getElementById("cui").value;
     var departamento = document.getElementById("departamento").value;
-    console.log("Nombre: " + nombre + " Apellido: " + apellido + " CUI: " + cui + " Departamento: " + departamento);
 
     var lista = document.getElementById('listado');
     var nodo = document.createElement("li");
@@ -16,4 +16,9 @@ function guardar() {
     mensaje.innerHTML = "Nombre: " + nombre + " Apellido: " + apellido + " CUI: " + cui + " Departamento: " + departamento;
     nodo.appendChild(mensaje);
     lista.appendChild(nodo);
+    
+    document.getElementById("nombre").value = '';
+    document.getElementById("apellido").value = '';
+    document.getElementById("cui").value = '';
+    document.getElementById("departamento").value = '';
 }
